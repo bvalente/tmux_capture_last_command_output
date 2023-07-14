6d8019951f823b27ee4d834ce9f9e41219407741
Bernardo Valente <bernardofvalente@gmail.com>
2023-07-14 10:03:57 +0100

    Remove custom right prompt

--- plugin.sh
+++ plugin.sh
@@ -4,8 +4,9 @@ x=$(tmux capture-pane -p -S '-' -J -t !)
 PROMPT_PATTERN=${PROMPT_PATTERN:-" ] % "}
 # tac inverts the text upside down
 # sed select everything from the second line (skip current prompt) until the next prompt inclusive
+# sed to remove the right hand prompt
 # tac to invert back
-result=$(echo "$x" | tac | gsed -n "2,/$PROMPT_PATTERN/p" | tac)
+result=$(echo "$x" | tac | gsed -n "2,/$PROMPT_PATTERN/p" | gsed -e "1 s/\([0-9]\{1,3\}\)m \(git:(.*)\)\{0,1\}$//"| tac)
 
 EDITOR_CMD=${EDITOR_CMD:-"$EDITOR -"}
 
