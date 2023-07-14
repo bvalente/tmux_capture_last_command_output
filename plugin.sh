27fe47bc6b538cbd73524746e43b8a33989ede05
Bernardo Valente <bernardofvalente@gmail.com>
2023-07-14 18:27:08 +0100

    Remove prompt trailing whitespaces

--- plugin.sh
+++ plugin.sh
@@ -6,7 +6,7 @@ PROMPT_PATTERN=${PROMPT_PATTERN:-" ] % "}
 # sed select everything from the second line (skip current prompt) until the next prompt inclusive
 # tac to invert back
 # sed to remove the right hand prompt of the first line
-result=$(echo "$x" | tac | gsed -n "2,/$PROMPT_PATTERN/p" | tac | gsed -e "1 s/\([0-9]\{1,3\}\)m \(git:(.*)\)\{0,1\}$//")
+result=$(echo "$x" | tac | gsed -n "2,/$PROMPT_PATTERN/p" | tac | gsed -e "1 s/\( \)*\([0-9]\{1,3\}\)m \(git:(.*)\)\{0,1\}$//")
 
 EDITOR_CMD=${EDITOR_CMD:-"$EDITOR -"}
 
